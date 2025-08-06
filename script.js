f808f439b31c1fb6bd0e55a7bdd584488ad7821d
Sudip Laha <sudiplaha.23@nshm.edu.in>
2025-08-07 00:15:48 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleAbout() {
-  const aboutSection = document.getElementById("about");
-  if (aboutSection.style.display === "none") {
-    aboutSection.style.display = "block";
-  } else {
-    aboutSection.style.display = "none";
-  }
-}
